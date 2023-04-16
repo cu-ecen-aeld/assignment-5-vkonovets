831f0761ad32555ae63fd8c01f8fdb609ce1fc93
vkonovets <slava.konovets@yahoo.com>
2023-04-16 21:49:54 +0200

    Bugfix

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8aea0d3'
+AESD_ASSIGNMENTS_VERSION = '4ec4ba7b9d3769517bc77f3ab9bd9d5bf5ee2334'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
